f5fcf76dc322d527c14611a72a27c536480fb4fe
P-fury <plutocamera@gmail.com>
2024-04-01 22:42:01 +0200

    ore django-froms usage done with percent daily

--- film_lifeapp/forms.py
+++ film_lifeapp/forms.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.safestring import SafeString
 from pycparser.ply.yacc import Production
 
-from film_lifeapp.models import ProductionHouse, Contact
+from film_lifeapp.models import ProductionHouse, Contact, Project, WorkDay, just_numb
 
 
 class RegisterUserForm(UserCreationForm):
@@ -56,12 +57,73 @@ class LoginForm(AuthenticationForm):
         })
 
 
+# ================= EDIT USER  ==================
 class EditUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name']
 
 
+# ================= EDIT PROJECT ==================
+class EditProjectForm(forms.ModelForm):
+    daily_rate = forms.IntegerField(min_value=1,
+                                    widget=forms.NumberInput(attrs={'step': 1}))
+
+    CHOICES = [
+        ('10', '10%'),
+        ('15', '15%'),
+        ('progresive', 'Progresive'),
+    ]
+
+    type_of_overhours = forms.ChoiceField(choices=CHOICES, required=False)
+
+    class Meta:
+        model = Project
+        fields = ['name', 'daily_rate', 'type_of_overhours', 'occupation', 'notes', 'production_house']
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('label_suffix', '')
+        super().__init__(*args, **kwargs)
+        self.fields['notes'].required = False
+        self.fields['production_house'].required = False
+
+    def clean_occupation(self):
+        occupation = self.cleaned_data['occupation']
+        if occupation is None:
+            return ""
+        return occupation
+
+
+# ================= EDIT WORK DAY ==================
+class EditWorkDayForm(forms.ModelForm):
+    date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}), required=True)
+    amount_of_overhours = forms.IntegerField(min_value=0, max_value=99, widget=forms.NumberInput(attrs={'step': 1}))
+    CHOICES = [
+        ('shooting day', 'shooting day'),
+        ('rehersal', 'rehersal'),
+        ('transport', 'transport'),
+        ('other', 'other')
+    ]
+    type_of_workday = forms.ChoiceField(choices=CHOICES, required=True)
+    percent_of_daily = forms.IntegerField(min_value=0, max_value=100)
+
+    class Meta:
+        model = WorkDay
+        fields = '__all__'
+        exclude = ['project', 'earnings']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['notes'].required = False
+        self.fields['percent_of_daily'].required = False
+        if 'type_of_workday' in self.initial and '% of daily rate' in self.initial['type_of_workday']:
+            percent = just_numb(self.initial['type_of_workday'])
+            self.initial['type_of_workday'] = 'other'
+            self.fields['percent_of_daily'].widget.attrs['style'] = 'display: block;'
+            self.initial['percent_of_daily'] = percent
+
+
+# ================= EDIT PRODUCTION ==================
 class EditProductionForm(forms.ModelForm):
     class Meta:
         model = ProductionHouse
